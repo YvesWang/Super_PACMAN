c1cf96403a8e42fbdc0adbf4e5750364630971b3
Tianyu Wang <yves.tyw@gmail.com>
2019-04-22 12:53:43 -0400

    GodGhost

--- ghostAgents.py
+++ ghostAgents.py
@@ -12,6 +12,7 @@ from game import Directions
 import random
 from util import manhattanDistance
 import util
+from SpathGhost import BFS
 
 class GhostAgent( Agent ):
   def __init__( self, index ):
@@ -59,6 +60,45 @@ class DirectionalGhost( GhostAgent ):
 
     # Select best actions given the state
     distancesToPacman = [manhattanDistance( pos, pacmanPosition ) for pos in newPositions]
+    if isScared:
+      bestScore = max( distancesToPacman )
+      bestProb = self.prob_scaredFlee
+    else:
+      bestScore = min( distancesToPacman )
+      bestProb = self.prob_attack
+    bestActions = [action for action, distance in zip( legalActions, distancesToPacman ) if distance == bestScore]
+    
+    # Construct distribution
+    dist = util.Counter()
+    for a in bestActions: dist[a] = bestProb / len(bestActions)
+    for a in legalActions: dist[a] += ( 1-bestProb ) / len(legalActions)
+    dist.normalize()
+    return dist
+
+
+class ShortestPathGhost( GhostAgent ):
+  "A ghost that prefers to rush Pacman, or flee when scared."
+  def __init__( self, index, prob_attack=0.8, prob_scaredFlee=0.8 ):
+    self.index = index
+    self.prob_attack = prob_attack
+    self.prob_scaredFlee = prob_scaredFlee
+      
+  def getDistribution( self, state ):
+    # Read variables from state
+    ghostState = state.getGhostState( self.index )
+    legalActions = state.getLegalActions( self.index )
+    pos = state.getGhostPosition( self.index )
+    isScared = ghostState.scaredTimer > 0
+    
+    speed = 1
+    if isScared: speed = 0.5
+    
+    actionVectors = [Actions.directionToVector( a, speed ) for a in legalActions]
+    newPositions = [( pos[0]+a[0], pos[1]+a[1] ) for a in actionVectors]
+    pacmanPosition = state.getPacmanPosition()
+
+    # Select best actions given the state
+    distancesToPacman = [BFS( pos, pacmanPosition) for pos in newPositions]
     if isScared:
       bestScore = max( distancesToPacman )
       bestProb = self.prob_scaredFlee
